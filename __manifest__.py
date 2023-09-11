28e535fc69db91ffebadf328f3f9b09dbcca6a9a
istiannas <annasrokie1@gmail.com>
2023-09-11 15:02:14 +0700

    @first_push

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'My Collection',
+    'version': '13.0.1',
+    'depends': ['base'],
+    'author': 'Ridwan & Jonatan',
+    'website': 'https://www.odoo.com/',
+    'summary': 'My Collection',
+    'data': [
+            # 'security/ir.model.access.csv',
+            # 'views/author_book_view.xml',
+            # 'views/category_book_view.xml',
+            # 'views/publisher_book_view.xml',
+            # 'data/menu_item.xml'
+        ],
+    'qweb': [],
+    'image': [],
+    'auto_install': False,
+    'installable': True,
+    'application': True,
+}
